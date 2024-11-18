f6f2282e61520d6ce3a8fb60fdd8730d26dea3d7
Alex Luhinin <barrrrygold@DESKTOP-NDOD1F8.localdomain>
2024-11-18 23:29:07 +0000

    Move list of plugins from init to plugins/packer

--- init.lua
+++ init.lua
@@ -1,36 +1,5 @@
 require('settings')
--- Plugin setup using packer.nvim (assuming you use packer instead of vim-plug)
-require('packer').startup(function(use)
-    -- List your plugins here
-    use 'wbthomason/packer.nvim'
-    use 'junegunn/fzf'
-    use 'junegunn/fzf.vim'
-    use 'morhetz/gruvbox'
-    use 'vim-airline/vim-airline'
-    use 'vim-airline/vim-airline-themes'
-    use 'tpope/vim-surround'
-    use 'raimondi/delimitmate'
-    use 'prabirshrestha/asyncomplete.vim'
-    use 'mfussenegger/nvim-dap'
-    use 'mfussenegger/nvim-dap-python'
-    use 'nvim-treesitter/nvim-treesitter'
-    use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
-    use 'tpope/vim-commentary'
-    use {'CopilotC-Nvim/CopilotChat.nvim', branch = 'canary', requires =  {'nvim-lua/plenary.nvim'}}
-    -- Nerdtree
-    use 'preservim/nerdtree'
-    use 'vimwiki/vimwiki'
-    use 'williamboman/mason.nvim'
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'L3MON4D3/LuaSnip'
-    use 'jose-elias-alvarez/null-ls.nvim'
-    use 'tpope/vim-fugitive'
-    
-
-    -- Additional plugins can be added similarly
-end)
+require('plugins.init')
 
 -- Configurations
 --
